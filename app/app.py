ee802a268dfa7cfc238dd63a2b5393f508f905a4
phillsearle <102220888+phillgames@users.noreply.github.com>
2025-04-09 10:47:06 +0200

    made a new app password

--- app/app.py
+++ app/app.py
@@ -24,7 +24,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'geir.translator.services@gmail.com'
-app.config['MAIL_PASSWORD'] = 'hbwj lnbx yeqh rife'
+app.config['MAIL_PASSWORD'] = 'xknd kreq ndcy iune'
 
 mail = Mail(app)
 
