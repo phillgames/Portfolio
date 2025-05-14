7e12a514014ffc60f72fe2cbbc4f3304232a08f8
phillsearle <102220888+phillgames@users.noreply.github.com>
2025-05-14 11:09:35 +0200

    moving around email and password to see if that changes anything

--- app/modules.py
+++ app/modules.py
@@ -35,7 +35,7 @@ class User(UserMixin):
         users = cur.fetchone()
         conn.close()
         if users:
-            return User(users['email'], users['password'], users['id'])
+            return User(users['password'], users['email'],  users['id'])
         return None
 
     @staticmethod
